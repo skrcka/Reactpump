4f2fd53d37d9a92a7115b4cda1e408162d22046d
Adam Krček <adam.krcek@avast.com>
2022-04-23 00:49:54 +0200

    moved screen lock where it belongs

--- src/App.jsx
+++ src/App.jsx
@@ -153,16 +153,6 @@ class App extends React.Component {
             <div className="App">
                 {/*<ReactInterval timeout={100} enabled={true} callback={this.checkForUpdates} />*/}
                 <div className='main m-1 p-1'>
-                    {screen_lock > 0 &&
-                        <div className='mymodal'>
-                            <Button color="warning" className='lockbutton' onClick={() => { this.setState({screen_lock: screen_lock-1}); }}>
-                                <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="100px" height="100px">
-                                    <path d="M 12 1 C 8.6761905 1 6 3.6761905 6 7 L 6 8 C 4.9069372 8 4 8.9069372 4 10 L 4 20 C 4 21.093063 4.9069372 22 6 22 L 18 22 C 19.093063 22 20 21.093063 20 20 L 20 10 C 20 8.9069372 19.093063 8 18 8 L 18 7 C 18 3.6761905 15.32381 1 12 1 z M 12 3 C 14.27619 3 16 4.7238095 16 7 L 16 8 L 8 8 L 8 7 C 8 4.7238095 9.7238095 3 12 3 z M 6 10 L 18 10 L 18 20 L 6 20 L 6 10 z M 12 13 C 10.9 13 10 13.9 10 15 C 10 16.1 10.9 17 12 17 C 13.1 17 14 16.1 14 15 C 14 13.9 13.1 13 12 13 z"/>
-                                </svg>
-                            </Button>
-                            <span>Click 3 times to unlock!</span>
-                        </div>
-                    }
                     <Row className="header">
                         <Col xs='3'>
                             <Button className='backbutton mt-1' disabled={mode === 0 || running} color="danger" onClick={() => {this.setState({ mode: 0} );}}>
@@ -178,6 +168,17 @@ class App extends React.Component {
                     </Row>
                     <div className='content text-left'>
                         {running &&
+                        <>
+                            {screen_lock > 0 &&
+                                <div className='mymodal'>
+                                    <Button color="warning" className='lockbutton' onClick={() => { this.setState({screen_lock: screen_lock-1}); }}>
+                                        <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="100px" height="100px">
+                                            <path d="M 12 1 C 8.6761905 1 6 3.6761905 6 7 L 6 8 C 4.9069372 8 4 8.9069372 4 10 L 4 20 C 4 21.093063 4.9069372 22 6 22 L 18 22 C 19.093063 22 20 21.093063 20 20 L 20 10 C 20 8.9069372 19.093063 8 18 8 L 18 7 C 18 3.6761905 15.32381 1 12 1 z M 12 3 C 14.27619 3 16 4.7238095 16 7 L 16 8 L 8 8 L 8 7 C 8 4.7238095 9.7238095 3 12 3 z M 6 10 L 18 10 L 18 20 L 6 20 L 6 10 z M 12 13 C 10.9 13 10 13.9 10 15 C 10 16.1 10.9 17 12 17 C 13.1 17 14 16.1 14 15 C 14 13.9 13.1 13 12 13 z"/>
+                                        </svg>
+                                    </Button>
+                                    <span>Click 3 times to unlock!</span>
+                                </div>
+                            }
                             <div className='maincontent'>
                                 <Row>
                                     <Col className='text-left'>
@@ -191,6 +192,7 @@ class App extends React.Component {
                                 </Row>
                                 <Progress name='progress' animated color="primary" value={progress} />
                             </div>
+                        </>
                         }
                         {!running &&
                             <div className='maincontent'>
